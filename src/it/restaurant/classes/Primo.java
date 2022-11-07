d3c9c9c373d72a4a6a969d61b3d36238bdc27caa
Marta Aramu <marta.aramu99@gmail.com>
2022-11-07 17:12:05 +0100

    correzione progetto

--- src/it/restaurant/classes/Primo.java
+++ src/it/restaurant/classes/Primo.java
@@ -1,8 +1,5 @@
 package it.restaurant.classes;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Primo extends Portata {
     public Primo(String name, double price) {
         super(name, price);
