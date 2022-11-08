fd9c2d50d3df9412013b92b579e206c42523eaec
Marta Aramu <marta.aramu99@gmail.com>
2022-11-08 14:55:17 +0100

    Correzione classe main

--- src/it/restaurant/classes/Main.java
+++ src/it/restaurant/classes/Main.java
@@ -13,14 +13,15 @@ public class Main {
         menu.addPortata(new Bevanda("Vino Rosso", 5.00));
         menu.addPortata(new Bevanda("Acqua frizzante", 1.50));
 
-        menu.addPortata(new Dessert("Sorbetto al limone", 5.00));
-        menu.addPortata(new Dessert("Parfait mandorle", 3.50));
-        menu.addPortata(new Dessert("Parfait pistacchio", 3.50));
-
-
         menu.addPortata(new Primo("Spaghetti alla carbonara",9));
         menu.addPortata(new Primo("Pappardelle al ragu di cinghiale",11));
         menu.addPortata(new Primo("Cannelloni al ragu",10));
+
+        //secondi
+
+        menu.addPortata(new Dessert("Sorbetto al limone", 5.00));
+        menu.addPortata(new Dessert("Parfait mandorle", 3.50));
+        menu.addPortata(new Dessert("Parfait pistacchio", 3.50));
 
         ristorante.printRistoranteDetails();
         menu.printMenuDetails();
