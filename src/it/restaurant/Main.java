377d2e0f8ccab824b35bed516102aff6a094ba8b
Dino <dino15031992@gmail.com>
2022-11-21 16:58:23 +0100

    modifiche: seconda modifica del main

--- src/it/restaurant/Main.java
+++ src/it/restaurant/Main.java
@@ -65,9 +65,9 @@ public class Main {
         System.out.println("\n--------------------------MENU BEVANDE ------------------------------------------------\n");
 
 
-        Portata lambrusco = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Lambrusco", 10.00);
-        Portata vinoRosso = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Vino Rosso", 5.00);
-        Portata acquaFrizzante = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Acqua frizzante", 1.50);
+        Portata lambrusco = new Bevanda("Bevanda", MenuTypesEnum.MENU_BEVANDE, "Lambrusco", 10.00);
+        Portata vinoRosso = new Bevanda("Bevanda", MenuTypesEnum.MENU_BEVANDE, "Vino Rosso", 5.00);
+        Portata acquaFrizzante = new Bevanda("Bevanda", MenuTypesEnum.MENU_BEVANDE, "Acqua frizzante", 1.50);
 
         menuBevande.add(lambrusco);
         menuBevande.add(vinoRosso);
@@ -85,11 +85,11 @@ public class Main {
         System.out.println("\n-------------------------- MENU BASE CARNE --------------------------------------------\n");
 
 
-        Portata spaghettiAllaCarbonara = new PrimoBaseCarne("Primo",MenuTypesEnum.MENU_BASECARNE, "Spaghetti alla carbonara", 9);
-        Portata pappardelleAlRaguDiCinghiale = new PrimoBaseCarne("Primo",MenuTypesEnum.MENU_BASECARNE, "Pappardelle al ragu di cinghiale", 11);
+        Portata spaghettiAllaCarbonara = new PrimoBaseCarne("Primo", MenuTypesEnum.MENU_BASECARNE, "Spaghetti alla carbonara", 9);
+        Portata pappardelleAlRaguDiCinghiale = new PrimoBaseCarne("Primo", MenuTypesEnum.MENU_BASECARNE, "Pappardelle al ragu di cinghiale", 11);
 
-        Portata cotoletta = new SecondoBaseCarne("Secondo",MenuTypesEnum.MENU_BASECARNE, "Cotoletta", 7.50);
-        Portata grigliataDiCinghiale = new SecondoBaseCarne("Secondo",MenuTypesEnum.MENU_BASECARNE, "Grigliata di cinghiale", 13.60);
+        Portata cotoletta = new SecondoBaseCarne("Secondo", MenuTypesEnum.MENU_BASECARNE, "Cotoletta", 7.50);
+        Portata grigliataDiCinghiale = new SecondoBaseCarne("Secondo", MenuTypesEnum.MENU_BASECARNE, "Grigliata di cinghiale", 13.60);
 
         menuBaseCarne.add(spaghettiAllaCarbonara);
         menuBaseCarne.add(pappardelleAlRaguDiCinghiale);
@@ -109,11 +109,11 @@ public class Main {
         System.out.println("\n-------------------------- MENU VEGETARIANO -------------------------------------------\n");
 
 
-        Portata carbonaraDiZucchine = new PrimoVegetariano("Primo",MenuTypesEnum.MENU_VEGETARIANO,"Carbonara di zucchine", 8.5);
-        Portata trofieAllaCremaDiPestoEnoci = new PrimoVegetariano("Primo",MenuTypesEnum.MENU_VEGETARIANO,"Trofie alla crema di pesto e noci", 11.50);
+        Portata carbonaraDiZucchine = new PrimoVegetariano("Primo", MenuTypesEnum.MENU_VEGETARIANO, "Carbonara di zucchine", 8.5);
+        Portata trofieAllaCremaDiPestoEnoci = new PrimoVegetariano("Primo", MenuTypesEnum.MENU_VEGETARIANO, "Trofie alla crema di pesto e noci", 11.50);
 
-        Portata funghiRipieniNonDiTritato = new SecondoVegetariano("Secondo",MenuTypesEnum.MENU_VEGETARIANO, "Funghi ripieni non di tritato", 10.40);
-        Portata zucchineGrigliate = new SecondoVegetariano("Secondo",MenuTypesEnum.MENU_VEGETARIANO, "Zucchine grigliate", 6.40);
+        Portata funghiRipieniNonDiTritato = new SecondoVegetariano("Secondo", MenuTypesEnum.MENU_VEGETARIANO, "Funghi ripieni non di tritato", 10.40);
+        Portata zucchineGrigliate = new SecondoVegetariano("Secondo", MenuTypesEnum.MENU_VEGETARIANO, "Zucchine grigliate", 6.40);
 
         menuVegetariano.add(carbonaraDiZucchine);
         menuVegetariano.add(trofieAllaCremaDiPestoEnoci);
@@ -124,3 +124,106 @@ public class Main {
         trofieAllaCremaDiPestoEnoci.portataInfo();
         funghiRipieniNonDiTritato.portataInfo();
         zucchineGrigliate.portataInfo();
+
+
+        /**
+         * Creazione Oggetti Primo e Secondo
+         * E aggiunzione degli oggetti PrimoVegano e SecondoVegano all' ArrayList menùVegano
+         */
+
+
+        System.out.println("\n-------------------------- MENU VEGANO ------------------------------------------------\n");
+
+
+        Portata risottoCastgneEcatalogna = new PrimoVegano("Primo", MenuTypesEnum.MENU_VEGANO, "Risotto castagne e catalogna", 14);
+        Portata casoncelliAllaBergamasca = new PrimoVegano("Primo", MenuTypesEnum.MENU_VEGANO, "Casoncelli alla bergamasca", 13);
+
+        Portata burgerVegani = new SecondoVegano("Secondo", MenuTypesEnum.MENU_VEGANO, "Burger Vegani", 8.40);
+        Portata grigliataVerdure = new SecondoVegano("Secondo", MenuTypesEnum.MENU_VEGANO, "Grigliata Verdure", 9.80);
+
+        menuVegano.add(risottoCastgneEcatalogna);
+        menuVegano.add(casoncelliAllaBergamasca);
+        menuVegano.add(burgerVegani);
+        menuVegano.add(grigliataVerdure);
+
+        risottoCastgneEcatalogna.portataInfo();
+        casoncelliAllaBergamasca.portataInfo();
+        burgerVegani.portataInfo();
+        grigliataVerdure.portataInfo();
+
+
+/**
+ * Creazione Oggetti Primo e Secondo
+ * E aggiunzione degli oggetti PrimoCeliaco e SecondoCeliaco all' ArrayList menùCeliaco
+ */
+
+
+        System.out.println("\n-------------------------- MENU GLUTENFREE --------------------------------------------\n");
+
+
+        Portata tagliatelleDiGranoSaraceno = new PrimoCeliaco("Primo", MenuTypesEnum.MENU_GLUTENFREE, "Tagliatelle di grano saraceno con ragu di prosciutto crudo", 12.50);
+        Portata gnocchiDiMiglio = new PrimoCeliaco("Primo", MenuTypesEnum.MENU_GLUTENFREE, "Gnocchi di miglio con asparagi e pancetta", 13.50);
+
+        Portata burgerDiMerluzzo = new SecondoCeliaco("Secondo", MenuTypesEnum.MENU_GLUTENFREE, "Burger di Merluzzo", 15.40);
+        Portata croccoleSenzaGlutine = new SecondoCeliaco("Secondo", MenuTypesEnum.MENU_GLUTENFREE, "Croccole senza Glutine ", 5.40);
+
+        menuGlutenFree.add(tagliatelleDiGranoSaraceno);
+        menuGlutenFree.add(gnocchiDiMiglio);
+        menuGlutenFree.add(burgerDiMerluzzo);
+        menuGlutenFree.add(croccoleSenzaGlutine);
+
+        tagliatelleDiGranoSaraceno.portataInfo();
+        gnocchiDiMiglio.portataInfo();
+        burgerDiMerluzzo.portataInfo();
+        croccoleSenzaGlutine.portataInfo();
+
+/**
+ * Creazione Oggetti Primo e Secondo
+ * E aggiunzione degli oggetti PrimoBambino e SecondoBambino all' ArrayList menùBambino
+ */
+
+
+        System.out.println("\n-------------------------- MENU BAMBINO -----------------------------------------------\n");
+
+
+        Portata farfalleConProsciutto = new PrimoBambino("Primo", MenuTypesEnum.MENU_BAMBINO, "Farfalle con prosciutto e piselli al forno", 8);
+        Portata vermicelliNeri = new PrimoBambino("Primo", MenuTypesEnum.MENU_BAMBINO, "Vermicelli neri con pomodori", 9);
+
+        Portata cotolettaPanata = new SecondoBambino("Secondo", MenuTypesEnum.MENU_BAMBINO, "Cotoletta Panata", 6.40);
+        Portata pizzaBaby = new SecondoBambino("Secondo", MenuTypesEnum.MENU_BAMBINO, "Pizza Baby", 8.50);
+
+        menuBambino.add(farfalleConProsciutto);
+        menuBambino.add(vermicelliNeri);
+        menuBambino.add(cotolettaPanata);
+        menuBambino.add(pizzaBaby);
+
+        farfalleConProsciutto.portataInfo();
+        vermicelliNeri.portataInfo();
+        cotolettaPanata.portataInfo();
+        pizzaBaby.portataInfo();
+
+/**
+ * Creazione Oggetti Dessert
+ * E aggiunzione degli oggetti Dessert all' ArrayList menùDessert
+ */
+
+
+        System.out.println("\n-------------------------- MENU DESSERT -----------------------------------------------\n");
+
+        Portata sorbettoAlLimone = new Dessert("Dessert", MenuTypesEnum.MENU_DESSERT, "Sorbetto al Limone", 5.00);
+        Portata parfaitMandorle = new Dessert("Dessert", MenuTypesEnum.MENU_DESSERT, "Parfait Mandorle", 3.50);
+        Portata parfaitPistacchi = new Dessert("Dessert", MenuTypesEnum.MENU_DESSERT, "Parfait Pistacchi", 5.00);
+        Portata cannoloScomposto = new Dessert("Dessert", MenuTypesEnum.MENU_DESSERT, "Cannolo Scomposto", 3.50);
+
+        menuDessert.add(sorbettoAlLimone);
+        menuDessert.add(parfaitMandorle);
+        menuDessert.add(parfaitPistacchi);
+        menuDessert.add(cannoloScomposto);
+
+        sorbettoAlLimone.portataInfo();
+        parfaitMandorle.portataInfo();
+        parfaitPistacchi.portataInfo();
+        cannoloScomposto.portataInfo();
+
+    }
+}
