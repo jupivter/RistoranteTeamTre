822babc67643837a939831ae031b49ae83179e54
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2022-11-21 16:51:55 +0100

    Giuseppe modificato il main

--- src/it/restaurant/Main.java
+++ src/it/restaurant/Main.java
@@ -6,69 +6,121 @@ import it.restaurant.portate.secondi.*;
 import it.restaurant.prenotazione.Cliente;
 import it.restaurant.prenotazione.ClienteTypesEnum;
 import it.restaurant.prenotazione.Tavolo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("-------------------------- RISTORANTE IL GHIOTTONE --------------------------------------\n");
 
         Ristorante ristorante = new Ristorante("Il ghiottone", "Via Indipendenza, 4",
                 "63733020363", "ilghiottone@gmail.com");
+        ristorante.printRistoranteInfo();
 
-        Menu menu = new Menu(MenuTypesEnum.BASECARNE);
+        /**
+         * Creazione ArrayList MenuList contenente altri ArrayList menu
+         * creazione ArrayList Menù diversificati per enumerazione
+         * aggiunzione ArrayList menu all'ArrayList menuList
+         */
+        ArrayList<ArrayList<Portata>> listMenu = new ArrayList<>();
 
-        Cliente cliente1 = new Cliente("Luca", "Rossi", ClienteTypesEnum.CARNIVORO, 327302658);
-        Cliente cliente2 = new Cliente("Marco", "Verdi",ClienteTypesEnum.CARNIVORO, 328302568);
-        Cliente cliente3 = new Cliente("Michele","Signorini",ClienteTypesEnum.CELIACO,333445661);
+        ArrayList<Portata> menuBevande = new ArrayList<>();
+        ArrayList<Portata> menuBaseCarne = new ArrayList<>();
+        ArrayList<Portata> menuVegetariano = new ArrayList<>();
+        ArrayList<Portata> menuVegano = new ArrayList<>();
+        ArrayList<Portata> menuGlutenFree = new ArrayList<>();
+        ArrayList<Portata> menuBambino = new ArrayList<>();
+        ArrayList<Portata> menuDessert = new ArrayList<>();
+        ArrayList<Tavolo> tavoliList = new ArrayList<>();
 
-        Tavolo tavolo1 = new Tavolo(01, 4,true);
-        Tavolo tavolo2 = new Tavolo(10, 8,true);
-        Tavolo tavolo3 = new Tavolo(4,3,true);
+        /**
+         * Predisposizione ArrayList di ordine clienti in base alla tipologia di menù scelta
+         */
+        List<Cliente> clientiMenuBevande = new ArrayList<>();
+        List<Cliente> clientiMenuBaseCarne = new ArrayList<>();
+        List<Cliente> clientiMenuVegetariano = new ArrayList<>();
+        List<Cliente> clientiMenuVegano = new ArrayList<>();
+        List<Cliente> clientiMenuGlutenFree = new ArrayList<>();
+        List<Cliente> clientiMenuBambino = new ArrayList<>();
+        List<Cliente> clientiMenuDessert = new ArrayList<>();
 
 
-        menu.addPortata(new Bevanda("Lambrusco", 10.00));
-        menu.addPortata(new Bevanda("Vino Rosso", 5.00));
-        menu.addPortata(new Bevanda("Acqua frizzante", 1.50));
-        // Primi di carne
-        menu.addPortata(new PrimoBaseCarne("Spaghetti alla carbonara",9));
-        menu.addPortata(new PrimoBaseCarne("Pappardelle al ragu di cinghiale",11));
-        // Primi vegetariani
-        menu.addPortata(new PrimoVegetariano("Carbonara di zucchine",8.5));
-        menu.addPortata(new PrimoVegetariano("Trofie alla crema di pesto e noci",11.50));
-        // Primi vegani
-        menu.addPortata(new PrimoVegano("Risotto castagne e catalogna",14));
-        menu.addPortata(new PrimoVegano("Casoncelli alla bergamasca",13));
-        // Primi celiaci
-        menu.addPortata(new PrimoCeliaco("Tagliatelle di grano saraceno con ragu di prosciutto crudo",12.50));
-        menu.addPortata(new PrimoCeliaco("Gnocchi di miglio con asparagi e pancetta",13.50));
-        // Primi bambino
-        menu.addPortata(new PrimoBambino("Farfalle con prosciutto e piselli al forno",8));
-        menu.addPortata(new PrimoBambino("Vermicelli neri con pomodori",9));
+        listMenu.add(menuBevande);
+        listMenu.add(menuBaseCarne);
+        listMenu.add(menuVegetariano);
+        listMenu.add(menuVegano);
+        listMenu.add(menuGlutenFree);
+        listMenu.add(menuBambino);
+        listMenu.add(menuDessert);
 
-        menu.addPortata(new SecondoBaseCarne("Cotoletta",7.50));
-        menu.addPortata(new SecondoBaseCarne("Grigliata di cinghiale",13.60));
-        menu.addPortata(new SecondoVegetariano("Funghi ripieni non di tritato",10.40));
-        menu.addPortata(new SecondoVegetariano("Zucchine grigliate",6.40));
-        menu.addPortata(new SecondoVegano("Burger Vegani",8.40));
-        menu.addPortata(new SecondoVegano("Grigliata Verdure",9.80));
-        menu.addPortata(new SecondoCeliaco("Burger di Merluzzo",15.40));
-        menu.addPortata(new SecondoCeliaco("Croccole senza Glutine ",5.40));
-        menu.addPortata(new SecondoBambino("Cotoletta Panata",6.40));
-        menu.addPortata(new SecondoBambino("Pizza Baby",8.50));
 
-        menu.addPortata(new Dessert("Sorbetto al limone", 5.00));
-        menu.addPortata(new Dessert("Parfait mandorle", 3.50));
-        menu.addPortata(new Dessert("Parfait pistacchio", 3.50));
+        /**
+         * Creazione Oggetti Bevanda
+         * E aggiunzione degli oggetti Bevanda all' ArrayList menùBevande
+         */
 
-        ristorante.printRistoranteDetails();
+        System.out.println("\n--------------------------MENU BEVANDE ------------------------------------------------\n");
 
-        menu.printMenuDetails();
 
-        System.out.println("-----------------------------AREA_PRENOTAZIONE-------------------------------------");
+        Portata lambrusco = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Lambrusco", 10.00);
+        Portata vinoRosso = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Vino Rosso", 5.00);
+        Portata acquaFrizzante = new Bevanda("Bevanda",MenuTypesEnum.MENU_BEVANDE, "Acqua frizzante", 1.50);
 
-        cliente1.prenota(cliente1,tavolo1);
+        menuBevande.add(lambrusco);
+        menuBevande.add(vinoRosso);
+        menuBevande.add(acquaFrizzante);
 
-        cliente2.prenota(cliente2,tavolo2);
+        lambrusco.portataInfo();
+        vinoRosso.portataInfo();
+        acquaFrizzante.portataInfo();
 
-        cliente3.prenota(cliente3,tavolo3);
-    }
-}
+        /**
+         * Creazione Oggetti Primo e Secondo
+         * E aggiunzione degli oggetti PrimoBaseCarne e SecondoBaseCarne all' ArrayList menùBaseCarne
+         */
+
+        System.out.println("\n-------------------------- MENU BASE CARNE --------------------------------------------\n");
+
+
+        Portata spaghettiAllaCarbonara = new PrimoBaseCarne("Primo",MenuTypesEnum.MENU_BASECARNE, "Spaghetti alla carbonara", 9);
+        Portata pappardelleAlRaguDiCinghiale = new PrimoBaseCarne("Primo",MenuTypesEnum.MENU_BASECARNE, "Pappardelle al ragu di cinghiale", 11);
+
+        Portata cotoletta = new SecondoBaseCarne("Secondo",MenuTypesEnum.MENU_BASECARNE, "Cotoletta", 7.50);
+        Portata grigliataDiCinghiale = new SecondoBaseCarne("Secondo",MenuTypesEnum.MENU_BASECARNE, "Grigliata di cinghiale", 13.60);
+
+        menuBaseCarne.add(spaghettiAllaCarbonara);
+        menuBaseCarne.add(pappardelleAlRaguDiCinghiale);
+        menuBaseCarne.add(cotoletta);
+        menuBaseCarne.add(grigliataDiCinghiale);
+
+        spaghettiAllaCarbonara.portataInfo();
+        pappardelleAlRaguDiCinghiale.portataInfo();
+        cotoletta.portataInfo();
+        grigliataDiCinghiale.portataInfo();
+
+        /**
+         * Creazione Oggetti Primo e Secondo
+         * E aggiunzione degli oggetti PrimoVegetariano e SecondoVegetariano all' ArrayList menùVegetariano
+         */
+
+        System.out.println("\n-------------------------- MENU VEGETARIANO -------------------------------------------\n");
+
+
+        Portata carbonaraDiZucchine = new PrimoVegetariano("Primo",MenuTypesEnum.MENU_VEGETARIANO,"Carbonara di zucchine", 8.5);
+        Portata trofieAllaCremaDiPestoEnoci = new PrimoVegetariano("Primo",MenuTypesEnum.MENU_VEGETARIANO,"Trofie alla crema di pesto e noci", 11.50);
+
+        Portata funghiRipieniNonDiTritato = new SecondoVegetariano("Secondo",MenuTypesEnum.MENU_VEGETARIANO, "Funghi ripieni non di tritato", 10.40);
+        Portata zucchineGrigliate = new SecondoVegetariano("Secondo",MenuTypesEnum.MENU_VEGETARIANO, "Zucchine grigliate", 6.40);
+
+        menuVegetariano.add(carbonaraDiZucchine);
+        menuVegetariano.add(trofieAllaCremaDiPestoEnoci);
+        menuVegetariano.add(funghiRipieniNonDiTritato);
+        menuVegetariano.add(zucchineGrigliate);
+
+        carbonaraDiZucchine.portataInfo();
+        trofieAllaCremaDiPestoEnoci.portataInfo();
+        funghiRipieniNonDiTritato.portataInfo();
+        zucchineGrigliate.portataInfo();
